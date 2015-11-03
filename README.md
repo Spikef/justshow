9f3412c14069f688c81aec5e0c6c92da402f1848
spikef <spikef@foxmail.com>
2015-11-03 15:50:10 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# justshow
-A pure html blog system based on nodejs cli.
+# JustShow
+
+一个基于node命令行工具构建的纯静态博客系统。
+
+A pure html blog system based on node cli.
---
+++ bin/show.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+// 默认输出内容
+console.log('A pure html blog system based on node cli.');
+console.log('Use the commands below to start your show!');
